320eb65ddceefd36bb841fb660fdeaf68b142c0c
Boris Ivanović <ivanovic.boris02@gmail.com>
2026-06-09 23:35:49 +0200

    Error Fixes & Adding MainLayout
    1. Installed Axios
    2. Fixed the  Property 'env' does not exist on type 'ImportMeta'.   ts(2339)  & Cannot find module 'vue-router' or its corresponding type declarations. ts(2307)
    3. Added MainLayout

---
+++ frontend/src/components/MainLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const year = new Date().getFullYear()
+</script>
+
+<template>
+    <nav style="background: #e0e0e0; padding: 12px;">
+        <div>
+            <RouterLink to="/" style="margin-right: 12px;">Početna</RouterLink>
+            <RouterLink to="/login">Prijava</RouterLink>
+        </div>
+    </nav>
+
+    <RouterView />
+
+    <footer style="margin-top: 20px; text-align: center;">
+        &copy; {{ year }} - Flower Store
+    </footer>
+</template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
+import Login from '@/pages/Login.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      meta: {
+        title: 'Početna'
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        title: 'Login'
+      }
+    }
+  ],
 })
 
 export default router
+
